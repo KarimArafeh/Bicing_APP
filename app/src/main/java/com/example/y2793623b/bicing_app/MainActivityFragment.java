0ca6629fea0351ee262a28a7645993e0e5f54cce
karim <y2793623b@iespoblenou.org>
2017-02-03 16:52:41 +0100

    Overlays

--- app/src/main/java/com/example/y2793623b/bicing_app/MainActivityFragment.java
+++ app/src/main/java/com/example/y2793623b/bicing_app/MainActivityFragment.java
@@ -1,24 +1,46 @@
 package com.example.y2793623b.bicing_app;
 
-import android.support.v4.app.Fragment;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import org.osmdroid.views.MapView;
 
+import org.osmdroid.api.IMapController;
+import org.osmdroid.bonuspack.clustering.RadiusMarkerClusterer;
+import org.osmdroid.bonuspack.overlays.Marker;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.MinimapOverlay;
+import org.osmdroid.views.overlay.ScaleBarOverlay;
+import org.osmdroid.views.overlay.compass.CompassOverlay;
+import org.osmdroid.views.overlay.compass.InternalCompassOrientationProvider;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
 
+
+
     public MainActivityFragment() {
     }
 
     private MapView map;
+    private IMapController mapController;
+    private MyLocationNewOverlay myLocationOverlay;
+    private ScaleBarOverlay mScaleBarOverlay;
+    private CompassOverlay mCompassOverlay;
+    private MinimapOverlay mMinimapOverlay;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -28,14 +50,79 @@ public class MainActivityFragment extends Fragment {
 
 
         map = (MapView) view.findViewById(R.id.mapView);
-        map.setTileSource(TileSourceFactory.MAPQUESTOSM);
-        map.setTilesScaledToDpi(true);
-        
-        map.setBuiltInZoomControls(true);
-        map.setMultiTouchControls(true);
 
-
+        initializeMap();
+        setZoom();
+        setOverlays();
+       
+        map.invalidate();
 
         return view;
     }
+
+
+
+    private void initializeMap() {
+
+
+
+        map.setTileSource(TileSourceFactory.DEFAULT_TILE_SOURCE);
+        map.setTilesScaledToDpi(true);
+
+        map.setBuiltInZoomControls(true);
+        map.setMultiTouchControls(true);
+
+    }
+
+
+
+    private void setZoom() {
+
+        //  Setteamos el zoom al mismo nivel y ajustamos la posición a un geopunto
+        mapController = map.getController();
+        mapController.setZoom(15);
+
+    }
+
+
+    private void setOverlays() {
+
+        final DisplayMetrics dm = getResources().getDisplayMetrics();
+
+
+        myLocationOverlay = new MyLocationNewOverlay(
+                getContext(),
+                new GpsMyLocationProvider(getContext()),
+                map
+        );
+        myLocationOverlay.enableMyLocation();
+        myLocationOverlay.runOnFirstFix(new Runnable() {
+            @Override
+            public void run() {
+                mapController.animateTo( myLocationOverlay.getMyLocation());
+            }
+        });
+
+
+        mScaleBarOverlay = new ScaleBarOverlay(map);
+        mScaleBarOverlay.setCentred(true);
+        mScaleBarOverlay.setScaleBarOffset(dm.widthPixels / 2, 10);
+
+        mCompassOverlay = new CompassOverlay(
+                getContext(),
+                new InternalCompassOrientationProvider(getContext()),
+                map
+        );
+        mCompassOverlay.enableCompass();
+
+        map.getOverlays().add(myLocationOverlay);
+        map.getOverlays().add(this.mMinimapOverlay);
+        map.getOverlays().add(this.mScaleBarOverlay);
+        map.getOverlays().add(this.mCompassOverlay);
+
+
+    }
+
+
+
 }
